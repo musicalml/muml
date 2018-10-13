a718f132835e1e747996861940d994c4ee746bba
SniXosha <admpavlov@yandex.ru>
2018-10-13 14:27:49 +0300

    Chords api added (#3)
    * /api/<id>/chords/ to get a list of chords in the song.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('track/', views.MidiList.as_view(), name='track'),
     path('track/<int:pk>/', views.MidiDetail.as_view(), name='track_detail'),
     path('track/<int:pk>/compare/', views.track_compare, name='compare_midisongs'),
+    path('track/<int:pk>/chords/', views.track_chords, name='track_chord'),
     path(r'drf/', include('rest_framework.urls', namespace='rest_framework')),
 ]
