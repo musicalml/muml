29ea9e4256ebdef6f19270d48a724aad9b76a357
Ilya Belkin <ilya.belkin-trade@yandex.ru>
2018-10-14 13:25:31 +0300

    Rearrange files for convenience

--- ml/bin/build/build.sh
+++ ml/bin/build/build.sh
@@ -5,6 +5,6 @@ echo "Start build"
 apt-get update
 
 pip3 install --upgrade pip
-pip3 install --no-cache-dir -r requirements.txt
+pip3 install --no-cache-dir -r build/requirements.txt
 
 mkdir ml repos databases thirdparty
