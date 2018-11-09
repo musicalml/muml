7e319dc40e46a167ced76968307acb4a28411e15
Ilya Belkin <ilya.belkin-trade@yandex.ru>
2018-11-09 13:42:59 +0300

    Add database for ml

--- database/init_db.sql
+++ database/init_db.sql
@@ -4,3 +4,5 @@ ALTER ROLE django SET client_encoding TO 'utf8';
 ALTER ROLE django SET default_transaction_isolation TO 'read committed';
 ALTER ROLE django SET timezone TO 'UTC';
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO django;
+
+CREATE DATABASE mldata;
