e8066959dea2a9afd8adf824dae52d274df05cc8
Ilya Belkin <ilya.belkin-trade@yandex.ru>
2018-11-10 22:44:14 +0300

    Fix run script

--- ml/run.sh
+++ ml/run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 python3 src/check_dependences.py \
-    & python3 src/midi_feature_extraction/migrate.py \
-    & python3 src/server/run.py
+    && python3 src/midi_feature_extraction/migrate.py \
+    && python3 src/server/run.py
